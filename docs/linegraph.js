25238a0649f1c6c531d86d0646094b97737e3e9b
Grace Handler <ghandler94@gmail.com>
2016-11-29 09:53:49 -0500

    working on making JSON

--- docs/linegraph.js
+++ docs/linegraph.js
@@ -1,7 +1,3 @@
-
-
-/* THIS SECTION PARSES DATA INTO THE RIGHT FORMAT FOR THE LINE GRAPH */
-
 var monthlyFiles = ["2015-08-01-tweets.csv", "2015-09-01-tweets.csv",
                     "2015-10-01-tweets.csv", "2015-11-01-tweets.csv",
                     "2015-12-01-tweets.csv", "2016-01-01-tweets.csv",
@@ -22,6 +18,9 @@ var numWords = 0; // total number of words
 var numHashtag = 0; // total number of moral words
 var numGeneral = 0; // total number of general words
 var numMoral = 0; // total number of moral words
+var classesFile = "classes.csv";
+var classes = [];
+var numUsers;
 
 /*
 
@@ -29,35 +28,92 @@ Reads in monthly fingerprint files, list of words of interest, and cleans up the
 REFERENCE: http://stackoverflow.com/questions/13870265/read-csv-tsv-with-no-header-line-in-d3
 
 */
-for (var i = 0; i < numFiles; i++) {
-  var curFile = monthlyFiles[i];
-  d3.text(curFile, function(text) {
-    var curDataset = d3.csv.parseRows(text);
-    dataset.push(curDataset);
-  });
+
+function readMonthCSV() {
+  console.log("BEGINNING MONTH CSV\n");
+  for (var i = 0; i < numFiles; i++) {
+    var curFile = monthlyFiles[i];
+    d3.text(curFile, function(text) {
+      var curDataset = d3.csv.parseRows(text);
+      dataset.push(curDataset);
+    });
+  }
+  console.log("ENDING MONTH CSV\n");
 }
 
-d3.text(wordFile, function(text) {
-  words = d3.csv.parseRows(text);
-  numWords = words[0].length;
-  for (var i = 0; i < numWords; i++) {
-    var curWord = words[0][i];
-    var curWordLength = curWord.length;
-    var curWordSuffix = curWord.substring(curWordLength-2, curWordLength);
-    if (curWordSuffix === "_M") {
-      numMoral = numMoral + 1;
-      curWord = curWord.substring(0, curWordLength-2);
-      moralWords.push(curWord);
-    } else if (curWordSuffix === "_H") {
-      numHashtag = numHashtag + 1;
-      curWord = curWord.substring(0, curWordLength-2);
-      hashtagWords.push(curWord);
-    } else {
-      numGeneral = numGeneral + 1;
-      generalWords.push(curWord);
+function readWordCSV() {
+  console.log("BEGINNING WORD CSV\n");
+  d3.text(wordFile, function(text) {
+    words = d3.csv.parseRows(text);
+    numWords = words[0].length;
+    for (var i = 0; i < numWords; i++) {
+      var curWord = words[0][i];
+      var curWordLength = curWord.length;
+      var curWordSuffix = curWord.substring(curWordLength-2, curWordLength);
+      if (curWordSuffix === "_M") {
+        numMoral = numMoral + 1;
+        curWord = curWord.substring(0, curWordLength-2);
+        moralWords.push(curWord);
+      } else if (curWordSuffix === "_H") {
+        numHashtag = numHashtag + 1;
+        curWord = curWord.substring(0, curWordLength-2);
+        hashtagWords.push(curWord);
+      } else {
+        numGeneral = numGeneral + 1;
+        generalWords.push(curWord);
+      }
+      allWords.push(curWord);
     }
-    allWords.push(curWord);
-  }
-});
+  });
+  console.log("ENDING WORD CSV");
+}
 
-/* THIS SECTION MAKES THE ACTUAL LINE GRAPH */
+function createJSON(dataset, wordset) {
+  console.log("BEGINNING JSON");
+  var class1 = [];
+  var class2 = [];
+  var class3 = [];
+  var class4 = [];
+  var class5 = [];
+  classes.push(class1);
+  classes.push(class2);
+  classes.push(class3);
+  classes.push(class4);
+  classes.push(class5);
+
+  d3.text(classesFile, function(text) {
+    var classData = words = d3.csv.parseRows(text);
+    numUsers = classData.length;
+    for (var i = 0; i < numUsers; i++) {
+      var index = classData[i][1];
+      classes[index].push(classData[i][0]);
+    }
+  });
+
+  var months = ["August2015", "September2015", "October2015", "November2015",
+                "December2015", "January2016", "February2016", "March2016",
+                "April2016", "May2016", "June2016", "July2016", "August2016",
+                "September2016", "October2016", "AllMonths"];
+  var numMonths = months.length;
+  var classNames = ["Class1", "Class2", "Class3", "Class4", "Class5", "AllClasses"];
+  var numClasses = classNames.length;
+  // words is called allWords
+  for (var m = 0; m < numMonths; m++) {
+    var tempClasses = [];
+    for (var c = 0; c < numClasses; c++) {
+      var tempWords = [];
+      for (var w = 0; w < 1; w++) {
+      }
+    }
+
+      /*monthsJSON.push({
+        month : months[m],
+        classes : classJSON
+      })*/
+  }
+  console.log("ENDING JSON\n");
+}
+
+readMonthCSV();
+readWordCSV();
+createJSON();
